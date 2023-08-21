238028c8ca3407a9933f0904a1eed84654fdde6c
pydev1996 <120152785+pydev1996@users.noreply.github.com>
2023-08-21 13:03:25 +0530

    commit

--- jobpositions/urls.py
+++ jobpositions/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     #path('', views.job_positions_list, name='job_positions_list'),
     path('', views.category_buttons, name='category_buttons'),
+    path('about/', views.about, name='about'),
     path('category/<str:category>/', views.job_positions_by_category, name='job_category'),
     # Add more URL patterns as needed
     path('job_positions/<int:job_position_id>/', views.job_position_detail, name='job_position_detail'),
