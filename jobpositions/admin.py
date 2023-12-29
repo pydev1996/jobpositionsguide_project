4a7be61d0cb716c5d7f667ed8f9339406e785bfc
pydev1996 <120152785+pydev1996@users.noreply.github.com>
2023-12-29 21:50:46 +0530

    updated

--- jobpositions/admin.py
+++ jobpositions/admin.py
@@ -1,9 +1,10 @@
 # jobpositionsguide_project/jobpositions/admin.py
 from django.contrib import admin
-from .models import JobPosition,ContactMessage,Institution,Billing
+from .models import JobPosition,ContactMessage,Institution,Billing,Institutor
 
 admin.site.register(JobPosition)
 admin.site.register(ContactMessage)
 admin.site.register(Billing)
 admin.site.register(Institution)
+admin.site.register(Institutor)
 
